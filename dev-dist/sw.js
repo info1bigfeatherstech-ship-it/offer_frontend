49d25a352724d25a6308696b2c4ce01ab3bd5d45
Tamanna209 <metamannaw3@gmail.com>
2026-06-01 07:06:49 +0530

    ship now label

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-caf3a6a6'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mllq4avhap"
+    "revision": "0.isksdoei90o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
