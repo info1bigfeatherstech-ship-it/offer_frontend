24903cfacee16357d5173fdfed15e41fb1a866fb
Tamanna209 <metamannaw3@gmail.com>
2026-06-01 19:31:22 +0530

    orders

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-caf3a6a6'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hvlf7d9s9po"
+    "revision": "0.3va0d553cpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
